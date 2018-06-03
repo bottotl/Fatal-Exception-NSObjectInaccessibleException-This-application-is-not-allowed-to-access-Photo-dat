4bfad8c5add790d3f62ccfe1a2cc0663609a717e
jft0m <377632523@qq.com>
2018-06-03 18:44:55 +0800

    update code

--- PhotosCrash/ViewController.m
+++ PhotosCrash/ViewController.m
@@ -11,11 +11,6 @@
 
 @interface ViewController ()
 @property (nonatomic, strong) NSMutableArray *photos;
-
-@property (nonatomic, strong) PHImageManager *manager;
-@property (nonatomic, strong) NSMutableArray *assets;
-@property (nonatomic, strong) dispatch_queue_t queue;
-@property (nonatomic, assign) BOOL stop;
 @end
 
 @implementation ViewController
@@ -23,59 +18,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.photos = @[].mutableCopy;
-
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-    [btn setTitle:@"按钮" forState:UIControlStateNormal];
-    btn.backgroundColor = [UIColor blackColor];
-    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
     
-    self.manager = [PHImageManager defaultManager];
-    PHFetchResult *result = [PHAsset fetchAssetsWithOptions:nil];
-    NSMutableArray *photos = @[].mutableCopy;;
-    for (PHAsset *asset in result) {
-        [photos addObject:asset];
+    [PHImageManager defaultManager];
+    while (1) {
+        UIImage *image = [UIImage imageNamed:@"1"];
+        NSData *data = UIImageJPEGRepresentation(image, 1);
+        [self.photos addObject:data];
     }
-    self.assets = photos;
-    
-//    [self.manager startCachingImagesForAssets:self.assets targetSize:CGSizeMake(300, 300) contentMode:PHImageContentModeDefault options:nil];
-    [self.assets enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if (idx>10) {
-            *stop = YES;
-        }
-        PHImageRequestOptions *options = [PHImageRequestOptions new];
-        options.networkAccessAllowed = YES;
-        [[PHImageManager defaultManager] requestImageForAsset:(PHAsset *)obj targetSize:PHImageManagerMaximumSize contentMode:PHImageContentModeDefault options:nil resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
-            
-        }];
-    }];
-    
-    self.queue = dispatch_queue_create("test", DISPATCH_QUEUE_SERIAL);
-    
-    dispatch_async(self.queue, ^{
-        while (1) {
-            if (self.stop) {
-                [NSThread sleepForTimeInterval:1];
-            }
-            
-            UIImage *image = [UIImage imageNamed:@"1"];
-            NSData *data = UIImageJPEGRepresentation(image, 1);
-            UIImage *newImage = [UIImage imageWithData:data];
-            [self.photos addObject:newImage];
-        }
-    });
 }
-
-- (void)btnClick:(id)sender {
-    NSLog(@"log ==== === ===");
-}
-
-- (void)didReceiveMemoryWarning {
-    self.stop = YES;
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    NSLog(@"memory warning");
-}
-
 
 @end
